5288c13eeb4c4b6c40f8e91f7fd48404e9466d51
meetsarvaiya2605 <meetsarvaiya41@gmail.com>
2025-03-06 17:16:29 +0530

    user updated

--- app/routers/user.py
+++ app/routers/user.py
@@ -15,3 +15,30 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.get("/{id}", response_model=schemas.UserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.delete("/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id ==id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="user not found")
+    user.delete(synchronize_session=False)
+    db.commit() 
+    return f"user {id} deleted successfull"
+
+@router.put("/{id}", response_model=schemas.UserResponse)
+def update_user(id:int, updated_user: schemas.UserCreate,db:Session = Depends(get_db)):
+    user_query = db.query(models.User).filter(models.User.id == id)
+    user = user_query.first()
+    if user ==  None:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                detail= f"post with id: {id} was not found")
+    user_query.update(updated_user.dict(),synchronize_session=False)
+    db.commit()   
+    return user_query.first()
