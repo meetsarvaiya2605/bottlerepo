005a32650e1c1d0aaba65aff49fb1be3e5200558
meetsarvaiya2605 <meetsarvaiya41@gmail.com>
2025-03-06 16:43:04 +0530

    update

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,18 +1,16 @@
-from fastapi import FastAPI ,HTTPException,Response, status,APIRouter,Depends
+from fastapi import FastAPI, HTTPException, Response, status, APIRouter, Depends
 
 from sqlalchemy.orm import Session
-import models,schemas
+import models, schemas
 from database import get_db
 import oauth2
 
-router = APIRouter(
-    prefix= "/users"
-)
+router = APIRouter(prefix="/users")
 
-@router.post("/",response_model= schemas.UserResponse)
-def create_user(user:schemas.UserCreate,db:Session=Depends(get_db)):
+
+@router.post("/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = models.User(**user.dict())
-
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
